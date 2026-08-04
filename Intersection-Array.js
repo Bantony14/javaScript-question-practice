0984077479a47e61033d5580f01e5e48ea0613c2
Bantony <bantonysin95@gmail.com>
2026-08-04 22:30:36 +0530

    Add array intersection implementations with duplicate handling

--- Intersection-Array.js
+++ Intersection-Array.js
@@ -1,7 +1,7 @@
 // Find common values from two arrays
 // Using one loop with includes()
 
-const arr1 = [3, 5, 7, 9, 11, 13];
+const arr1 = [3, 3, 5, 7, 9, 11, 13];
 const arr2 = [1, 3, 6, 7, 10, 11, 15];
 const result = [];
 
@@ -14,16 +14,12 @@ for (let i = 0; i < arr1.length; i++) {
 console.log("result:", result);
 
 // Find common values using nested loops (without includes())
-// This approach also prevents duplicate values in the result
 
 const result2 = [];
 
 for (let i = 0; i < arr1.length; i++) {
   for (let j = 0; j < arr2.length; j++) {
     if (arr1[i] === arr2[j]) {
-      if (result2.includes(arr1[i])) {
-        break;
-      }
       result2.push(arr1[i]);
       break;
     }
@@ -31,3 +27,24 @@ for (let i = 0; i < arr1.length; i++) {
 }
 
 console.log("result2:", result2);
+
+// Find common values using nested loops
+// Track duplicates with an object (without using includes() or Set)
+const obj = {};
+const result3 = [];
+
+for (let i = 0; i < arr1.length; i++) {
+  for (let j = 0; j < arr2.length; j++) {
+    if (arr1[i] === arr2[j]) {
+      obj[arr1[i]] = obj[arr1[i]] ? obj[arr1[i]] + 1 : 1;
+      if (obj[arr1[i]] > 1) {
+        break;
+      }
+      result3.push(arr1[i]);
+      break;
+    }
+  }
+}
+console.log(obj);
+
+console.log("result3:", result3);
