a4b66b52558040c4bb750807546d3723573a3f57
Bantony <bantonysin95@gmail.com>
2026-08-25 21:09:16 +0530

    understading of prototype and contruction function

---
+++ PrototypeWithCntrnFunc.js
@@ -0,0 +1,16 @@
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const user1 = new User("bantony", 24);
+const user2 = new User("antony", 26); // User.call(user1,"bantony", 24)
+
+User.prototype.sayHello = function () {
+  console.log("heloo", this.name);
+};
+
+const user3 = {};
+
+console.log(user1.sayHello());
+console.log(user2.sayHello());
