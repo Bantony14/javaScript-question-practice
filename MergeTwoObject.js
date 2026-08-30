8f82ee16554aeac37dfbfb91608dea655e76ea9e
Bantony <bantonysin95@gmail.com>
2026-08-30 21:12:55 +0530

    shallow merge

---
+++ MergeTwoObject.js
@@ -0,0 +1,45 @@
+// const obj = {
+//   name: "Bantony",
+//   age: 24,
+//   address: {
+//     contry: "india",
+//     city: "surat",
+//     state: "gujarat",
+//   },
+// };
+
+// const obj2 = {
+//   name: "Antony",
+//   dign: "IT",
+// };
+
+// const shallowCombine = { ...obj, ...obj2 };
+
+// shallowCombine.address.city = "vapi";
+
+// console.log(obj);
+// console.log(shallowCombine);
+
+// const obj1 = {
+//   name: "Rahul",
+//   address: {
+//     city: "Delhi",
+//   },
+// };
+
+// const obj2 = {
+//   age: 25,
+//   address: {
+//     pincode: 1234,
+//   },
+// };
+
+// function deepMerge(obj1, obj2) {
+//   let deepMergeObj = {};
+
+//   let length = Math.max(obj1.length, obj2.length);
+
+//   for (let i = 0; i < length - 1; i++) {
+
+//   }
+// }
